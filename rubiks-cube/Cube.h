e4d95de24798851e50282b522a730775b9ebdb94
Taz Greenwood <tazgreenwood@Tazs-MacBook-Pro.local>
2017-08-23 09:06:35 -0600

    Initial Commit

---
+++ rubiks-cube/Cube.h
@@ -0,0 +1,13 @@
+//
+//  Cube.h
+//  rubiks-cube
+//
+//  Created by Taz Greenwood on 8/23/17.
+//  Copyright © 2017 tazgreenwood. All rights reserved.
+//
+
+#ifndef Cube_h
+#define Cube_h
+
+
+#endif /* Cube_h */
---
+++ rubiks-cube/rubiks-cube/Cube.cpp
@@ -0,0 +1,9 @@
+//
+//  Cube.cpp
+//  rubiks-cube
+//
+//  Created by Taz Greenwood on 8/23/17.
+//  Copyright © 2017 tazgreenwood. All rights reserved.
+//
+
+#include "Cube.hpp"
